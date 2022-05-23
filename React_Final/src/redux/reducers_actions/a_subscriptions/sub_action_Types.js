f8de4a3830b5dc42c8db3709e9958e48955fc586
Mayan Asif Levy <58429034+MayanAsifLevy@users.noreply.github.com>
2022-05-23 09:17:12 +0300

    Delete React_Final directory

--- React_Final/src/redux/reducers_actions/a_subscriptions/sub_action_Types.js
+++
@@ -1,3 +0,0 @@
-export const GET_MEMBERS = "GET_MEMBERS";
-export const GET_MEMBER = "GET_MEMBER";
-export const PAGE_STATE="PAGE_STATE";
--- React_Final/webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports= {
-resolve:{
-    fallback:{ url: require.resolve("url/") },
-},
-
-};
